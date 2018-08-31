36594463d088f91a5238a30d8696f547df2cec36
JasonD <jasonedou@gmail.com>
2018-08-31 15:02:00 -0600

    css edits
    central div is not pushing page longer...unsure why

--- js/animations.js
+++ js/animations.js
@@ -1,9 +1,43 @@
 $(document).ready( function(e){
+        //$('.imagetray').slick({
+        //    infinite: true,
+        //    slidesToShow: 4,
+         //   slidesToScroll: 2
+        //});
         $('.imagetray').slick({
+                    dots: true,
             infinite: true,
+            speed: 300,
             slidesToShow: 4,
-            slidesToScroll: 2
-        });
+            slidesToScroll: 2,
+            responsive: [
+                {
+                breakpoint: 1124,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+                },
+                {
+                breakpoint: 820,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+                },
+                {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]});
 
     var $window           = $(window),
         win_height_padded = $window.height() * 1.2,
